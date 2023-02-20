509609744d241bf951e3f244051f16aa400a3d39
takahiro-sawada <zeroshift.seed@gmail.com>
2023-02-20 15:11:31 +0900

    jsx-react17-mod

--- my-app/routes/index.tsx
+++ my-app/routes/index.tsx
@@ -1,24 +1,54 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+/** @jsxmportSource h/react */
+import { h, PageProps } from "$fresh/runtime.ts"; // ①
+import { Head } from "$fresh/src/runtime/head.ts";
+import { Handlers } from "$fresh/server.ts";
 
-export default function Home() {
+interface Article {
+  id: string;
+  title: string;
+  created_at: string;
+}
+
+export const handler: Handlers<Article[]> = { // ②
+  async GET(_, ctx) {
+    const articles: Article[] = [
+      {
+        id: "1",
+        title: "Article 1",
+        created_at: "2022-06-17T00:00:00.000Z",
+      },
+      {
+        id: "2",
+        title: "Article 2",
+        created_at: "2022-06-10T00:00:00.000Z",
+      },
+    ];
+    return ctx.render(articles);
+  },
+};
+
+export default function Home({ data }: PageProps<Article[]>) { // ③
   return (
-    <>
+    <div>
       <Head>
-        <title>Fresh App</title>
+        <title>Fresh Blog</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+      <div>
+        <h1>Fresh Blog</h1>
+        <section>
+          <h2>Posts</h2>
+          <ul>
+            {data.map((article) => (
+              <li key={article.id}>
+                <a href={`articles/${article.id}`}>
+                  <h3>{article.title}</h3>
+                  <time dateTime={article.created_at}>{article.created_at}</time>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
-    </>
+    </div>
   );
 }
